8a102e22ab93f91783027b53bb8f941253b407e2
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-05-04 23:43:51 +0600

    env

--- src/AuthProvider/app.js
+++ src/AuthProvider/app.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBcATeKL2hjxnM1ikXIQ-RyPaRM8noRhKQ",
-  authDomain: "chef-project-c5fc0.firebaseapp.com",
-  projectId: "chef-project-c5fc0",
-  storageBucket: "chef-project-c5fc0.appspot.com",
-  messagingSenderId: "891078657828",
-  appId: "1:891078657828:web:d51fb3efb1f96a258255b1"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
